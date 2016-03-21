15ee4cad8d2db5b4afa3c29216a4de69caf2e17f
troy <chou804@gmail.com>
2016-03-21 09:14:40 +0800

    part II ok

--- LinqProviderTest1/Program.cs
+++ LinqProviderTest1/Program.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using LinqToProvider;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace LinqProviderTest1
 {
@@ -10,6 +13,26 @@ namespace LinqProviderTest1
     {
         static void Main(string[] args)
         {
+            string constr = @"Data Source=Northwind.db3;Version=3;";
+            using (var con = new SQLiteConnection(constr))
+            {
+                con.Open();
+                Northwind db = new Northwind(con);
+
+                IQueryable<Customers> query =
+                     db.Customers.Where(c => c.City == "London");
+
+                Console.WriteLine("Query:\n{0}\n", query);
+
+                var list = query.ToList();
+                foreach (var item in list)
+                {
+                    Console.WriteLine("Name: {0}", item.ContactName);
+                }
+
+                Console.ReadLine();
+            }
+
         }
     }
 }
